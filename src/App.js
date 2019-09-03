9d4b3305af013b2574ffb4061fe4922d1475be13
Timothy McGowen <mcgowen17@yahoo.com>
2019-09-02 22:06:34 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export const asyncFunc = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Success!')
+    }, 1000)
+  })
+}
+
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import banana from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
 
-import App from './App';
+import App, { asyncFunc } from './App';
 
 describe('<App />', () => {
   // 2. write this test
   it('matches snapshot', () => {
-    const tree = banana.create(<App />); // generates a DOM tree
+    const tree = renderer.create(<App />); // generates a DOM tree
 
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
   });
 });
+
+describe('asyncFunc', () => {
+  it('eventually resolves to success', () => {
+    const resolvedValue = null
+    const expected = 'Success!'
+    asyncFunc().then(res => {
+      resolvedValue = res
+      expected(resolvedValue).toEqual(expected)
+    })
+  })
+})
+
